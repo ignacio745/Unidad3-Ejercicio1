1508401938c130a38164298a2e9931850c0ea68d
ignacio745 <62523458+ignacio745@users.noreply.github.com>
2022-05-13 16:46:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from ManejadorFacultades import ManejadorFacultades
 
 if __name__ == "__main__":
     unManejadorFacultades = ManejadorFacultades()
-    unManejadorFacultades.cargarFacultades("Facultades malo.csv")
+    unManejadorFacultades.cargarFacultades("Facultades.csv")
     unMenu = Menu()
     unMenu.ejecutarMenu(unManejadorFacultades)
