397518480fc996c6e0b2068e91c1e825a3430926
ignacio745 <62523458+ignacio745@users.noreply.github.com>
2022-05-27 21:27:05 -0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from Menu import Menu
-from ManejadorFacultades import ManejadorFacultades
-
-if __name__ == "__main__":
-    unManejadorFacultades = ManejadorFacultades()
-    unManejadorFacultades.cargarFacultades("Facultades.csv")
-    unMenu = Menu()
-    unMenu.ejecutarMenu(unManejadorFacultades)
